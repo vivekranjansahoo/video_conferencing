4a8e93e50d4848cfa6656ddf35fe46dfebb9342a
Dharmaraj <dharmarajjena694@gmail.com>
2024-08-21 14:47:33 +0530

    Socket handler completed

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,8 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { MongodbConfig } from './mongo.config';
+import { ConfigModule } from '@nestjs/config';
+import webrtcConfig from './webrtc.config';
 
 @Global()
 @Module({
+  imports: [ConfigModule.forRoot({
+    load: [webrtcConfig],
+  })],
   providers: [...MongodbConfig],
   exports: [...MongodbConfig],
 })
--- src/webrtc/webrtc.controller.ts
+++ src/webrtc/webrtc.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { WebrtcService } from './webrtc.service';
 
 @Controller('webrtc')
-export class WebrtcController {}
+export class WebrtcController {
+
+    constructor(private webrtcService:WebrtcService) {}
+
+    @Post('create-room')
+    createRoom(@Body() data:{ userId: string }) : string {
+        // console.log(userId)
+        // if(!userId)
+        //     throw Error("Expected userId");
+        const { userId } =data;
+        const roomId=this.webrtcService.createRoom(userId);
+        return roomId;
+    }
+
+
+
+}
